ae545a007d291be2b349c5289bb82ade0dbfddea
Huanmai <huannv@itomosoft.com>
2025-05-07 20:59:44 +0700

    PUSH NOTIFICATION

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,40 @@
 import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'Core/Router/app_routes.dart';
 import 'firebase_options.dart';
+@pragma('vm:entry-point')
+Future<void> requestNotificationPermission() async {
+  FirebaseMessaging messaging = FirebaseMessaging.instance;
 
+  NotificationSettings settings = await messaging.requestPermission(
+    alert: true,
+    badge: true,
+    sound: true,
+  );
+
+  if (settings.authorizationStatus == AuthorizationStatus.authorized) {
+    print('✅ User granted permission');
+  } else if (settings.authorizationStatus == AuthorizationStatus.provisional) {
+    print('ℹ️ User granted provisional permission');
+  } else {
+    print('❌ User declined or has not accepted permission');
+  }
+}
+
+Future<void> _firebaseMessagingBackgroundHandler(RemoteMessage message) async {
+  await Firebase.initializeApp();
+  print('📩 (Background) Message received: ${message.notification?.title}');
+}
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  FirebaseMessaging.onBackgroundMessage(_firebaseMessagingBackgroundHandler);
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
+
   );
+  await requestNotificationPermission();
   runApp(const MyApp());
 }
 
@@ -19,7 +45,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return GetMaterialApp(
       debugShowCheckedModeBanner: false,
-      title: "Chien K12CNTT03",
+      title: "Muscic App",
       initialRoute: AppRoutes.splash,
       getPages: AppPages.routes,
     );
